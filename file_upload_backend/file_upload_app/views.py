ccd37e2061ec8045d5ca180532457b730ba970f6
Rômulo Dusi <dusiromulo@gmail.com>
2019-09-07 19:09:42 -0300

    insert user name in request

--- file_upload_backend/file_upload_app/views.py
+++ file_upload_backend/file_upload_app/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import User
 import os
+import errno
 import json
 
 
@@ -10,18 +11,23 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def handle_uploaded_file(file, filename):
     path_to_upload = os.path.join(dir_path, '..', 'uploads', filename)
+    if not os.path.exists(os.path.dirname(path_to_upload)):
+        try:
+            os.makedirs(os.path.dirname(path_to_upload))
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
     with open(path_to_upload, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
-            print("CHUCK WITH SIZE " + str(chunk))
         destination.close()
 
 
 @csrf_exempt
 def upload_file(request):
     if request.method == 'POST':
-        if 'file' in request.FILES and 'filename' in request.POST:
-            handle_uploaded_file(request.FILES['file'], request.POST['filename'])
+        if 'file' in request.FILES and 'filename' in request.POST and 'username' in request.POST:
+            handle_uploaded_file(request.FILES['file'], request.POST['username'] + '/' + request.POST['filename'])
             data = json.dumps({})
             return HttpResponse(data, status=200, content_type="application/json")
         return HttpResponse("", status=400, content_type="application/json")
