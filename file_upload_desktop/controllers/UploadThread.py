0779763f98062e9a18f77babda1096b6dffb4630
Rômulo Dusi <dusiromulo@gmail.com>
2019-09-07 14:03:32 -0300

    fix threads funcionallity

--- file_upload_desktop/controllers/UploadThread.py
+++ file_upload_desktop/controllers/UploadThread.py
@@ -1,22 +1,30 @@
 import time
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 
 
 class UploadThread(QThread):
-    sig_new_percentage = pyqtSignal(str)
+    sig_new_percentage = pyqtSignal(int, int)
+    sig_cancel_upload = pyqtSignal()
     percentage = 0
 
     def __init__(self, parent=None, file_path=""):
         QThread.__init__(self, parent)
         self.file_path = file_path
         self.running = False
+        self.sig_cancel_upload.connect(self.on_cancel_upload)
 
+    @pyqtSlot()
     def on_cancel_upload(self):
         self.running = False
 
     def run(self):
         self.running = True
         while self.running:
-            self.percentage += 1
-            self.sig_new_percentage.emit(self.percentage)
+            total_diff = 2
+            self.percentage += total_diff
+            if self.percentage >= 100:
+                self.sig_new_percentage.emit(100, total_diff)
+                break
+            else:
+                self.sig_new_percentage.emit(self.percentage, total_diff)
             time.sleep(1)
